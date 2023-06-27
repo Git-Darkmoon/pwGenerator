6ec6053dffd09d35253664b05126b6f132295b7d
Nicolas Mayorga <namayorgav@gmail.com>
2023-06-27 18:04:26 -0500

    feat: generatePassword function but gives error.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react"
 import { FaRegCopy } from "react-icons/fa"
+import { AiOutlineArrowRight } from "react-icons/ai"
 
 function Card() {
+  const [pwLength, setPwLength] = useState(1)
+
+  const [password, setPassword] = useState("- - - - - -")
+  const [useLowerCase, setUseLowerCase] = useState(false)
+  const [useUpperCase, setUseUpperCase] = useState(false)
+  const [useNumbers, setUseNumbers] = useState(false)
+  const [useSymbols, setUseSymbols] = useState(false)
+
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numbers = "0123456789"
+  const symbols = `!@#$%^&*()_-+=[]{};:'",.<>/?|~"`
+
+  function generatePassword(length) {
+    let characters = ""
+    if (useLowerCase) characters += lowerCase
+    if (useUpperCase) characters += upperCase
+    if (useNumbers) characters += numbers
+    if (useSymbols) characters += symbols
+
+    let generatedPassword = ""
+    const passwordLength = length // Desired password length
+
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length)
+      generatedPassword += characters[randomIndex]
+    }
+
+    setPassword(generatedPassword)
+  }
+
   return (
     <article className="generator-card">
       <div className="generator-pw">
-        <h2 id="pwGenerated"> ---------</h2>
+        <h2 id="pwGenerated"> {password}</h2>
         <FaRegCopy />
       </div>
       <br />
       <div className="generator-specs">
         <div className="generator-info">
-          <h3 id="charTxt">character lenght</h3>
-          <h2 id="charLength">1</h2>
+          <h3 id="charTxt">character length</h3>
+          <h2 id="charLength">{pwLength}</h2>
         </div>
         <input
           type="range"
@@ -20,6 +53,10 @@ function Card() {
           step={1}
           min={1}
           max={20}
+          value={pwLength}
+          onChange={(e) => {
+            setPwLength(e.target.value)
+          }}
         />
         <section className="generator-optionContainer">
           <div className="option">
@@ -28,6 +65,8 @@ function Card() {
               name=""
               id="lowerCase"
               className="generator-option"
+              checked={useLowerCase}
+              onChange={() => setUseLowerCase(!useLowerCase)}
             />
             <label htmlFor="lowerCase">include lowercase letters</label>
           </div>
@@ -37,6 +76,8 @@ function Card() {
               name=""
               id="upperCase"
               className="generator-option"
+              checked={useUpperCase}
+              onChange={() => setUseUpperCase(!useUpperCase)}
             />
             <label htmlFor="upperCase">include uppercase letters</label>
           </div>
@@ -46,6 +87,8 @@ function Card() {
               name=""
               id="numbers"
               className="generator-option"
+              checked={useNumbers}
+              onChange={() => setUseNumbers(!useNumbers)}
             />
             <label htmlFor="numbers">include numbers</label>
           </div>
@@ -55,6 +98,8 @@ function Card() {
               name=""
               id="symbols"
               className="generator-option"
+              checked={useSymbols}
+              onChange={() => setUseSymbols(!useSymbols)}
             />
             <label htmlFor="symbols">include symbols</label>
           </div>
@@ -62,16 +107,21 @@ function Card() {
         <section className="generator-strength">
           <h1>Strength</h1>
           <meter
-            id="fuel"
+            id="pwStrength-meter"
             min="0"
-            max="10"
-            low="3"
-            high="6"
-            optimum="8"
-            value="6"
+            max="20"
+            low="8"
+            high="14"
+            optimum="20"
+            value={pwLength}
           />
         </section>
-        <button className="generator-generateBtn">Generate </button>
+        <button
+          className="generator-generateBtn"
+          onClick={console.log(generatePassword(pwLength))}
+        >
+          Generate <AiOutlineArrowRight />
+        </button>
       </div>
     </article>
   )
